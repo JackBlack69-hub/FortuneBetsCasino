5dbbf31963f6beb61c12c66fd7aed4929c58b293
Himanshu Sonawane <himanshusonawane1345@gmail.com>
2023-08-28 02:06:19 +0530

    Table css and buttons placement changed

--- client/src/components/chat/BasicTable.jsx
+++ client/src/components/chat/BasicTable.jsx
@@ -7,10 +7,36 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import tableCrash from "../../assets/tableCrash.png";
+import tableJackpot from "../../assets/tableJackpot.png"
+import tableRoullete from "../../assets/tableRoullete.png"
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
+    borderRadius: 20,
+    backgroundColor: "#001724",
+    padding: 20,
+  },
+  tableRow: {
+    backgroundColor: "#021E2C",
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  tableCell: {
+    color: "white",
+  },
+  gamesCell: {
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: 10,
+  },
+  gamesImage: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
   },
 });
 
@@ -19,42 +45,85 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Crash", 159, 9648784.25 , 24, 4121032616356),
+  createData("Jackpot", 237, 9.0, 37, 4.3),
+  createData("Crash", 262, 16.0, 24, 6.0),
+  createData("Roullete", 305, 3.7, 67, 4.3),
+  createData("Jackpot", 356, 16.0, 49, 3.9),
 ];
+
+const imagesMap = {
+  Crash: tableCrash,
+  Jackpot: tableJackpot,
+  Roullete: tableRoullete,
+};
 
 export default function BasicTable() {
   const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
+    <>
+      <h1 style={{ color: "#fff", fontWeight: 400 }}>Latest bets</h1>
+      <TableContainer component={Paper} className={classes.table}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.tableCell} align="left">
+                Game
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell className={classes.tableCell} align="left">
+                Player
+              </TableCell>
+              <TableCell className={classes.tableCell} align="left">
+                Bet amount
+              </TableCell>
+              <TableCell className={classes.tableCell} align="left">
+                Multiplier
+              </TableCell>
+              <TableCell className={classes.tableCell} align="right">
+                Profit amount
+              </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.name} className={classes.tableRow}>
+                <TableCell className={classes.tableCell} align="left">
+                  <div className={classes.gamesCell}>
+                    <img
+                      className={classes.gamesImage}
+                      src={imagesMap[row.name]}
+                      alt={`${row.name} Icon`}
+                    />
+                    <span>{row.name}</span>
+                  </div>
+                </TableCell>
+                <TableCell className={classes.tableCell} align="left">
+                  {row.calories}
+                </TableCell>
+                <TableCell className={classes.tableCell} align="left">
+                  {row.fat}
+                </TableCell>
+                <TableCell className={classes.tableCell} align="left">
+                  <Button
+                    style={{
+                      backgroundColor: "#05FF0033",
+                      color: "#00D758",
+                      height: 24,
+                      minWidth: 65,
+                    }}
+                  >
+                    {row.carbs}x
+                  </Button>
+                </TableCell>
+                <TableCell className={classes.tableCell} align="right">
+                  {row.protein}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
--- client/src/components/chat/SwipeableGames.jsx
+++ client/src/components/chat/SwipeableGames.jsx
@@ -2,55 +2,44 @@ import React from "react";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import MobileStepper from "@material-ui/core/MobileStepper";
 import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
 import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
-import CasinoUI from "./CasinoUI";
-import Casino_coins from "../../assets/Casino_coins.png";
-import Casino_rectangle from "../../assets/Casino_rectangle.png";
-import Casino2 from "../../assets/Casino2.png";
-import poker_table from "../../assets/Poker_table.png";
 import jackpotImage from "../../assets/games-jackpot.png";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: "100%",
-    // flexGrow: 1,
     background: "inherit",
-  },
-
-  img: {
-    height: 255,
-    display: "block",
-    maxWidth: 400,
-    overflow: "hidden",
-    width: "100%",
   },
   paper: {
     display: "flex",
     flexWrap: "wrap",
-
     "& > *": {
       margin: theme.spacing(3),
       width: theme.spacing(16),
       height: theme.spacing(16),
     },
   },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: theme.spacing(2),
+  },
 }));
 
 export function imageComponent() {
   return (
-    <>
-      <img
-        src={jackpotImage}
-        style={{ paddingLeft: "2%", paddingRight: "2%" }}
-      ></img>
-    </>
+    <img
+      src={jackpotImage}
+      style={{ paddingLeft: "2%", paddingRight: "2%" }}
+      alt="Jackpot"
+    />
   );
 }
 
@@ -61,76 +50,67 @@ function SwipeableGames() {
   const maxSteps = 2;
 
   const handleNext = () => {
-    setActiveStep(prevActiveStep => prevActiveStep + 1);
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
   const handleBack = () => {
-    setActiveStep(prevActiveStep => prevActiveStep - 1);
-  };
-
-  const handleStepChange = step => {
-    setActiveStep(step);
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
   return (
     <div className={classes.root}>
-      <AutoPlaySwipeableViews
+      <div className={classes.header}>
+        <h1 style={{ color: "#fff", fontWeight: 400 }}>games</h1>
+        <MobileStepper
+          style={{backgroundColor:'transparent'}}
+          steps={maxSteps}
+          position="static"
+          variant={null}
+          activeStep={activeStep}
+          nextButton={
+            <Button
+              size="small"
+              onClick={handleNext}
+              style={{ color: "rgba(255, 255, 255, 0.5)" }}
+              disabled={activeStep === maxSteps - 1}
+            >
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowLeft />
+              ) : (
+                <KeyboardArrowRight />
+              )}
+            </Button>
+          }
+          backButton={
+            <Button
+              style={{ color: "rgba(255, 255, 255, 0.5)" }}
+              size="small"
+              onClick={handleBack}
+              disabled={activeStep === 0}
+            >
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowRight />
+              ) : (
+                <KeyboardArrowLeft />
+              )}
+            </Button>
+          }
+        />
+      </div>
+
+      <SwipeableViews
         axis={theme.direction === "rtl" ? "x-reverse" : "x"}
         index={activeStep}
-        onChangeIndex={handleStepChange}
+        onChangeIndex={setActiveStep}
         enableMouseEvents
       >
         <div>
-          <Paper component={imageComponent} style={classes.paper} />
-
-          <Paper component={imageComponent} style={classes.paper} />
-
-          <Paper component={imageComponent} style={classes.paper} />
+          <Paper component={imageComponent} className={classes.paper} />
         </div>
         <div>
-          <Paper component={imageComponent} style={classes.paper} />
-          <Paper component={imageComponent} style={classes.paper} />
-
-          <Paper component={imageComponent} style={classes.paper} />
+          <Paper component={imageComponent} className={classes.paper} />
         </div>
-      </AutoPlaySwipeableViews>
-
-      <MobileStepper
-        style={{ justifyContent: "flex-end", gap: 2 }}
-        className={classes.root}
-        steps={maxSteps}
-        position="static"
-        variant={null}
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            style={{ color: "white" }}
-            disabled={activeStep === maxSteps - 1}
-          >
-            {theme.direction === "rtl" ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button
-            style={{ color: "white" }}
-            size="small"
-            onClick={handleBack}
-            disabled={activeStep === 0}
-          >
-            {theme.direction === "rtl" ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-          </Button>
-        }
-      />
+      </SwipeableViews>
     </div>
   );
 }
