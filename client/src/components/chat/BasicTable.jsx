3240320128c58c54c7f1528432ab68fa6d4d1a40
Himanshu Sonawane <himanshusonawane1345@gmail.com>
2023-08-28 22:37:01 +0530

    table css

--- client/src/components/chat/BasicTable.jsx
+++ client/src/components/chat/BasicTable.jsx
@@ -7,9 +7,6 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import tableCrash from "../../assets/tableCrash.png";
-import tableJackpot from "../../assets/tableJackpot.png";
-import tableRoullete from "../../assets/tableRoullete.png";
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
@@ -17,8 +14,6 @@ const useStyles = makeStyles({
     minWidth: 650,
     borderRadius: 20,
     backgroundColor: "#001724",
-    padding: 20,
-    margin: "2%",
     "& .MuiTable-root": {
       border: "none",
     },
@@ -29,19 +24,17 @@ const useStyles = makeStyles({
     marginBottom: 20,
     backgroundColor: "#021E2C",
     "& .MuiTable-root": {
-      border: "none",
+      border: "none", 
     },
   },
   tableCell: {
     color: "white",
     border: "none",
-
-    padding: "2%",
+    padding: 20,
   },
   gamesCell: {
     display: "flex",
     alignItems: "center",
-
     paddingLeft: 10,
   },
   gamesImage: {
@@ -51,85 +44,64 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Crash", 159, 9648784.25, 24, 4121032616356),
-  createData("Jackpot", 237, 9.0, 37, 4.3),
-  createData("Crash", 262, 16.0, 24, 6.0),
-  createData("Roullete", 305, 3.7, 67, 4.3),
-  createData("Jackpot", 356, 16.0, 49, 3.9),
-];
-
-const imagesMap = {
-  Crash: tableCrash,
-  Jackpot: tableJackpot,
-  Roullete: tableRoullete,
-};
-
-export default function BasicTable() {
+export default function BasicTable(props) {
   const classes = useStyles();
+
+  const { columns, data, imagesMap } = props;
 
   return (
     <div style={{ margin: "2%" }}>
-      <h1 style={{ color: "#fff", fontWeight: 400 }}>Latest bets</h1>
+      <h1 style={{ color: "#fff", fontWeight: 400 }}>{props.title}</h1>
       <TableContainer component={Paper} className={classes.table}>
         <Table aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell className={classes.tableCell} align="left">
-                Game
-              </TableCell>
-              <TableCell className={classes.tableCell} align="left">
-                Player
-              </TableCell>
-              <TableCell className={classes.tableCell} align="left">
-                Bet amount
-              </TableCell>
-              <TableCell className={classes.tableCell} align="left">
-                Multiplier
-              </TableCell>
-              <TableCell className={classes.tableCell} align="right">
-                Profit amount
-              </TableCell>
+              {columns.map((column, index) => (
+                <TableCell
+                  key={index}
+                  style={{ padding: 20 }}
+                  className={classes.tableCell}
+                  align="left"
+                >
+                  {column}
+                </TableCell>
+              ))}
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map(row => (
-              <TableRow key={row.name} className={classes.tableRow}>
-                <TableCell className={classes.tableCell} align="left">
-                  <div className={classes.gamesCell}>
-                    <img
-                      className={classes.gamesImage}
-                      src={imagesMap[row.name]}
-                      alt={`${row.name} Icon`}
-                    />
-                    <span>{row.name}</span>
-                  </div>
-                </TableCell>
-                <TableCell className={classes.tableCell} align="left">
-                  {row.calories}
-                </TableCell>
-                <TableCell className={classes.tableCell} align="left">
-                  {row.fat}
-                </TableCell>
-                <TableCell className={classes.tableCell} align="left">
-                  <Button
-                    style={{
-                      backgroundColor: "#05FF0033",
-                      color: "#00D758",
-                      height: 24,
-                      minWidth: 65,
-                    }}
+            {data.map((row, rowIndex) => (
+              <TableRow key={rowIndex} className={classes.tableRow}>
+                {row.map((cell, cellIndex) => (
+                  <TableCell
+                    key={cellIndex}
+                    className={classes.tableCell}
+                    align="left"
                   >
-                    {row.carbs}x
-                  </Button>
-                </TableCell>
-                <TableCell className={classes.tableCell} align="right">
-                  {row.protein}
-                </TableCell>
+                    {cellIndex === 0 ? (
+                      <div className={classes.gamesCell}>
+                        <img
+                          className={classes.gamesImage}
+                          src={imagesMap[cell]}
+                          alt={`${cell} Icon`}
+                        />
+                        <span>{cell}</span>
+                      </div>
+                    ) : cellIndex === 3 ? (
+                      <Button
+                        style={{
+                          backgroundColor: "#05FF0033",
+                          color: "#00D758",
+                          height: 24,
+                          minWidth: 65,
+                        }}
+                      >
+                        {cell}x
+                      </Button>
+                    ) : (
+                      cell
+                    )}
+                  </TableCell>
+                ))}
               </TableRow>
             ))}
           </TableBody>
--- client/src/components/chat/CasinoUITest.jsx
+++ client/src/components/chat/CasinoUITest.jsx
@@ -6,6 +6,9 @@ import BasicTable from "./BasicTable";
 import { Paper } from "@material-ui/core";
 import jackpotImage from "../../assets/games-jackpot.png";
 import SwipeableGames from "./SwipeableGames";
+import tableCrash from "../../assets/tableCrash.png"
+import tableJackpot from "../../assets/tableJackpot.png"
+import tableRoullete from "../../assets/tableRoullete.png"
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -26,7 +29,22 @@ export default function CasinoUITest() {
     <div style={{}}>
       <SwipeableTextMobileStepper />
       <SwipeableGames style={{ margin: "5%" }} />
-      <BasicTable />
+      <BasicTable
+        title="Latest bets"
+        columns={["Game", "Player", "Bet amount", "Multiplier", "Profit amount"]}
+        data={[
+          ["Crash", 159, 9648784.25, 24, 4121032616356],
+          ["Jackpot", 237, 9.0, 37, 4.3],
+          ["Crash", 262, 16.0, 24, 6.0],
+          ["Roullete", 305, 3.7, 67, 4.3],
+          ["Jackpot", 356, 16.0, 49, 3.9],
+        ]}
+        imagesMap={{
+          Crash: tableCrash,
+          Jackpot: tableJackpot,
+          Roullete: tableRoullete,
+        }}
+      />
     </div>
   );
 }
